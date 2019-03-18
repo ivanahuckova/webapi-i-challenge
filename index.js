e25411b18d7c308c23e1c1f5e7bf95387d8d86b9
Ivana Huckova <ivana.huckova@gmail.com>
2019-03-18 15:36:05 +0100

    Set up post method

--- index.js
+++ index.js
@@ -7,14 +7,21 @@ const port = 8000;
 
 server.use(express.json());
 
-server.get('/', (req, res) => {
-  res.send('Hello World');
-});
-
 server.post('/api/users', (req, res) => {
-  db.update({ name: req.body.name, bio: req.body.bio }).then(data => {
-    res.json(data);
-  });
+  if (req.body.name && req.body.bio) {
+    db.insert(req.body)
+      .then(() => {
+        res.status(201);
+        db.findById(id).then(newUser => res.json(newUser));
+      })
+      .catch(() => {
+        res.status(500);
+        res.send({ error: 'There was an error while saving the user to the database' });
+      });
+  } else {
+    res.status(400);
+    res.send({ errorMessage: 'Please provide name and bio for the user.' });
+  }
 });
 
 server.listen(port, () => console.log(`Listening on port 8000`));
