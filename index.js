949fc79cb9a80d10499a855aa2f4e7586fbb4c3c
Ivana Huckova <ivana.huckova@gmail.com>
2019-03-18 15:28:04 +0100

    Set up port

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // implement your API here
+const express = require('express');
+const server = express();
+const db = require('./data/db.js');
+
+const port = 8000;
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+server.post('/api/users', (req, res) => {
+  db.update({ name: req.body.name, bio: req.body.bio }).then(data => {
+    res.json(data);
+  });
+});
+
+server.listen(port, () => console.log(`Listening on port 8000`));
